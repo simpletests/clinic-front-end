3f02f2972cc7858e8604b30d4c3345cfa4beb38c
tomaslm <tomaslm@hotmail.com>
2017-05-15 23:33:54 -0300

    Melhoria nas visualizações semanais e diaria

--- src/app/calendar/calendar-daily-view/calendar-daily-view.component.ts
+++ src/app/calendar/calendar-daily-view/calendar-daily-view.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, OnChanges } from '@angular/core';
+import { Observable } from "rxjs/Observable";
 
 @Component({
     selector: 'app-calendar-daily-view',
     templateUrl: './calendar-daily-view.component.html',
     styleUrls: ['./calendar-daily-view.component.scss']
 })
-export class CalendarDailyViewComponent implements OnInit {
+export class CalendarDailyViewComponent implements OnInit, OnChanges {
 
     hoursTier: any[];
 
     @Input() day;
-    constructor() { }
+    constructor() {
+    }
 
     ngOnInit() {
-        this.hoursTier = [];
-        for (let i = 0; i < 24; i++) {
-            this.hoursTier[i] = {
-                label: i <= 12 ? i + ' am' : i - 11 + ' pm',
-                events: this.findEventsByHour(this.day.events, i)
-            };
+        this.fillHoursTier();
+    }
+
+    ngOnChanges() {
+        this.fillHoursTier();
+    }
+
+    private fillHoursTier() {
+        if (this.day && this.day.events) {
+            let minHours = 8;
+            let maxHours = 18;
+            this.hoursTier = [];
+            for (let i = minHours; i < maxHours; i++) {
+                this.hoursTier.push({
+                    label: i <= 12 ? i + ':00 am' : i - 12 + ':00 pm',
+                    events: this.findEventsByHour(this.day.events, i)
+                });
+            }
         }
     }
 
--- src/app/calendar/calendar-weekly-view/calendar-weekly-view.component.ts
+++ src/app/calendar/calendar-weekly-view/calendar-weekly-view.component.ts
@@ -1,16 +1,47 @@
-import {Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'app-calendar-weekly-view',
     templateUrl: './calendar-weekly-view.component.html',
     styleUrls: ['./calendar-weekly-view.component.scss']
 })
-export class CalendarWeeklyViewComponent implements OnInit {
+export class CalendarWeeklyViewComponent implements OnInit, OnChanges {
 
-    @Input() week;
-    constructor() {}
+    hoursDaysTier;
+    @Input() week = [];
+    constructor() { }
 
     ngOnInit() {
+    }
+    ngOnChanges() {
+        this.fillHoursTier();
+    }
+
+    private fillHoursTier() {
+        if (this.week) {
+            this.hoursDaysTier = [];
+            let minHours = 8;
+            let maxHours = 18;
+            for (let i = minHours; i < maxHours; i++) {
+                this.hoursDaysTier[i] = [];
+                for (let day of this.week) {
+                    this.hoursDaysTier[i][day.date.getDay()] = {
+                        label: i <= 12 ? i + ':00 am' : i - 12 + ':00 pm',
+                        events: this.findEventsByHour(day.events, i)
+                    };
+                }
+            }
+        }
+    }
+
+    private findEventsByHour(events: any[], hour: number): any[] {
+        let response: any[] = [];
+        for (let event of events) {
+            if (event.start.getHours() == hour) {
+                response.push(event);
+            }
+        }
+        return response;
     }
 
 }
