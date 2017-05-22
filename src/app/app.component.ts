25cc59552892b06a4e78a9a2f4eb1fb56594432a
Acer01 <Acer01@ailogdev03>
2017-05-22 08:46:51 -0300

    Remocao do attendance do menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -9,13 +9,12 @@ export class AppComponent {
     title = 'app works! ';
 
     navItems = [
-        {route: "login", name: "Login", icon: "vpn_key"},
-        {route: "dashboard", name: "Dashboard", icon: "home"},
-        {route: "attendance", name: "Attendance", icon: "perm_contact_calendar"},
-        {route: "calendar", name: "Calendar", icon: "date_range"},
-        {route: "patient", name: "Patient", icon: "person"},
-        {route: "financial", name: "Financial", icon: "attach_money"},
-        {route: "report", name: "Report", icon: "description"},
-        {route: "user", name: "User", icon: "sentiment_very_dissatisfied"}
+        { route: "login", name: "Login", icon: "vpn_key" },
+        { route: "dashboard", name: "Dashboard", icon: "home" },
+        { route: "calendar", name: "Calendar", icon: "date_range" },
+        { route: "patient", name: "Patient", icon: "person" },
+        { route: "financial", name: "Financial", icon: "attach_money" },
+        { route: "report", name: "Report", icon: "description" },
+        { route: "user", name: "User", icon: "sentiment_very_dissatisfied" }
     ];
 }
