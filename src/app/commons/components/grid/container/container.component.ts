74a06df7d1fe4fb50dffd2cd90acdbf2d972618f
Wesley Rocha <wesleyallanrocha@gmail.com>
2017-06-01 18:35:38 -0300

    Continuando a criação dos componentes para a responsividade.

--- src/app/commons/components/grid/container/container.component.ts
+++ src/app/commons/components/grid/container/container.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit, Renderer, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, Renderer, ElementRef, Input, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'ss-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
-export class ContainerComponent implements OnInit {
-
+export class ContainerComponent implements OnInit, AfterViewInit {
+  
+  @ViewChild('container') div: ElementRef;
   @Input() fluid: boolean = true;
 
   constructor(private el: ElementRef, private renderer: Renderer) { }
 
   ngOnInit() {
-    if (this.fluid) {
-      this.renderer.setElementClass(this.el.nativeElement, 'container-fluid', true);
-    } else {
-      this.renderer.setElementClass(this.el.nativeElement, 'container', true);
-    }
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.div);
   }
 }
