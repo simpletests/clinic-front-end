563dbff0a93405cd139866671a902f7949693830
wesley rocha <wesley rocha>
2017-06-03 01:21:28 -0300

    Terminado o input básico, de texto e senha, com atributos placeholder e name, e [(ngModel)].

--- src/app/commons/components/input/input.component.ts
+++ src/app/commons/components/input/input.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, forwardRef, OnInit, Input, ElementRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from "@angular/forms";
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => InputComponent),
+  multi: true
+}
 
 @Component({
   selector: 'ss-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACESSOR]
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements ControlValueAccessor {
 
+  @Input('type') type: string;
   @Input('isTextarea') isTextarea: boolean = false;
   @Input('name') name: string;
   @Input('placeholder') placeholder: string;
 
-  constructor() { }
+  private innerValue: any = '';
+  private onChangeCallback: (_: any) => void = () => {};
+  private onTouchedCallback: () => void = () => {};
 
-  ngOnInit() {
+  get value() { return this.innerValue; }
+  set value(value) {
+    this.innerValue = value;
+    this.onChangeCallback(value);
   }
 
+  writeValue(value: any): void {
+    this.innerValue = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    throw new Error("Method not implemented.");
+  }
 }
